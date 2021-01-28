2e332af85f614e1675986550a261f31f0aff2eb4
Deathik4 <zolotov2189@mail.ru>
2021-01-28 10:28:53 +0300

    added follower section and fixed week section

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,18 @@ $(function () {
         starWidth: "12px",
     });
 
+    $(".follower__items").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '.follower__arrows-left',
+        nextArrow: '.follower__arrows-right'
+    });
+
+    $(".week__content").slick({
+        prevArrow: '.week__arrows-left',
+        nextArrow: '.week__arrows-right'
+    });
+
     var mixer = mixitup('.release__items');
 });
